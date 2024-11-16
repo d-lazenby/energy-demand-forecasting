99d615993d511ad498e5923fa3e12c0609d632c7
Doug <douglazenby@gmail.com>
2024-11-16 06:53:21 +0000

    Tidying data collection notebooks; moving data wrangling functions to data.py

---
+++ src/data.py
@@ -0,0 +1,105 @@
+import calendar
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+import requests
+import pandas as pd
+
+from src.paths import PARENT_DIR, RAW_DATA_DIR, TRANSFORMED_DATA_DIR
+
+load_dotenv(PARENT_DIR / ".env")
+EIA_API_KEY = os.environ["EIA_API_KEY"]
+
+
+def download_raw_data(year: int, month: int):
+    """
+    Downloads one month of raw data from EIA for {year}, {month}.
+    
+    Args:
+        year: Year of data collection
+        month: Month of data collection
+    """
+    file_path = RAW_DATA_DIR / f"demand_{year}_{month}.csv"
+    if file_path.exists():
+        print(f"File demand_{year}_{month}.csv exists locally already, try next URL")
+    else:
+        # Need the number of days in the current (year, month)
+        _, num_days = calendar.monthrange(year, month)
+
+        URL = (
+            "https://api.eia.gov/v2/electricity/rto/daily-region-data/data/"
+            "?frequency=daily"
+            "&data[0]=value"
+            "&facets[timezone][]=Eastern"
+            "&facets[type][]=D"
+            f"&start={year}-{month:02d}-01"
+            f"&end={year}-{month:02d}-{num_days}"
+            "&sort[0][column]=period"
+            "&sort[0][direction]=desc"
+            "&offset=0"
+            "&length=5000"
+            f"&api_key={EIA_API_KEY}"
+        )
+
+        response = requests.get(url=URL).json()["response"]["data"]
+        data = pd.DataFrame(response)
+
+        # Tidies dataframe and saves to csv
+        data = data[["period", "respondent", "value"]].copy()
+        data.rename(
+            columns={
+                "period": "datetime",
+                "value": "demand",
+                "respondent": "ba_code",
+            },
+            inplace=True,
+        )
+
+        data.to_csv(file_path, index=False)
+        print(
+            f"Data for {year}_{month} successfully downloaded to demand_{year}_{month}.csv"
+        )
+
+def prepare_raw_data_for_training():
+    """
+    Concatentates all raw data and prepares it for training. 
+    Missing values filled with -1 for now. 
+    """
+    concat_demand = pd.DataFrame(columns=["datetime", "ba_code", "demand"])
+
+    for file_path in RAW_DATA_DIR.glob("*.csv"):
+        with open(file_path, "rb"):
+            tmp = pd.read_csv(file_path)
+            concat_demand = pd.concat([concat_demand, tmp])
+
+    # To deal with downcasting when filling NaNs
+    concat_demand["demand"] = concat_demand["demand"].astype(int)
+
+    # For annotating the file name
+    min_month, min_year = (
+        datetime.strptime(concat_demand["datetime"].min(), "%Y-%m-%d").month,
+        datetime.strptime(concat_demand["datetime"].min(), "%Y-%m-%d").year,
+    )
+
+    max_month, max_year = (
+        datetime.strptime(concat_demand["datetime"].max(), "%Y-%m-%d").month,
+        datetime.strptime(concat_demand["datetime"].max(), "%Y-%m-%d").year,
+    )
+
+    data = pd.pivot_table(
+        data=concat_demand, values="demand", index="datetime", columns="ba_code"
+    )
+    # Resetting column names
+    data.columns.name = None
+    data.columns = [f"ba_{ba_code}" for ba_code in data.columns]
+
+    # Filling missing values with -1 for now
+    data = data.fillna(-1)
+
+    data = data.sort_index()
+
+    data.to_csv(
+        TRANSFORMED_DATA_DIR
+        / f"ts_tabular_{min_year}_{min_month}_to_{max_year}_{max_month}.csv"
+    )
