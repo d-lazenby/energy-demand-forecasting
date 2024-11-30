2610acedfc9faa92bdb2a77e61bd52ca3608969b
Doug <douglazenby@gmail.com>
2024-11-30 20:23:44 +0000

    Adding in hopsworks and feature store constants

--- src/config.py
+++ src/config.py
@@ -1,3 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+from src.paths import PARENT_DIR
+
+load_dotenv(PARENT_DIR / ".env")
+
+try:
+    HOPSWORKS_API_KEY = os.getenv("HOPSWORKS_API_KEY")
+except:
+    raise Exception("Create a .env file on the project root with the HOPSWORKS_API_KEY")
+
+HOPSWORKS_PROJECT_NAME = os.getenv("HOPSWORKS_PROJECT_NAME")
+
+FEATURE_GROUP_NAME = "daily_demand_feature_group"
+FEATURE_GROUP_VERSION = 1
+
 # BAs for which we also have map layers: 53 in total.
 BAS = [
     "AZPS",
