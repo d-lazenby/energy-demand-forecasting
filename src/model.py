1edb6c7dde682c48a7a54a33af1f097d35d07807
Doug <douglazenby@gmail.com>
2025-03-13 13:21:18 +0000

    refactor: feature-engineeering classes moved to model.py and docstrings added; metric modified to MAE

--- src/model.py
+++ src/model.py
@@ -1,8 +1,133 @@
 import pandas as pd
 import numpy as np
-from typing import Tuple
+from typing import Tuple, Optional
 
+from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+
+class FeatureEngineerByBA(BaseEstimator, TransformerMixin):
+    """
+    Applies a given transformer to each unique {ba_code} group in the dataset.
+
+    This class loops over the unique values of {ba_code} in the dataset and applies
+    the provided transformer independently to each group. The fitted transformers
+    are stored for later transformations.
+
+    Attributes:
+        transformer: The transformation to be applied.
+        fitted_transformers: Stores fitted transformers per {ba_code}.
+    """
+
+    def __init__(self, transformer: TransformerMixin) -> None:
+        """
+        Initializes the FeatureEngineerByBA with a given transformer.
+
+        Args:
+            transformer: The transformer to be applied to each BA time series.
+        """
+        self.transformer = transformer
+        self.fitted_transformers: dict[str, TransformerMixin] = {}
+
+    def fit(self, X: pd.DataFrame, y: Optional[pd.Series] = None) -> "FeatureEngineerByBA":
+        """
+        Fits the transformer to each {ba_code} time series.
+
+        Args:
+            X: Input dataset.
+            y: Target variable.
+
+        Returns:
+            The fitted instance.
+        """
+        for ba_code in X["ba_code"].unique():
+            tmp = X.loc[X["ba_code"] == ba_code, :].copy()
+            self.fitted_transformers[ba_code] = self.transformer.fit(tmp)
+        return self
+
+    def transform(self, X: pd.DataFrame, y: Optional[pd.Series] = None) -> pd.DataFrame:
+        """
+        Transforms the dataset using the fitted transformers for each {ba_code} time series.
+
+        Args:
+            X: Input dataset.
+            y: Target variable.
+
+        Returns:
+            The transformed dataset.
+        """
+        output = pd.DataFrame()
+        for ba_code in X["ba_code"].unique():
+            tmp = X.loc[X["ba_code"] == ba_code, :].copy()
+            tmp = self.fitted_transformers[ba_code].transform(tmp)
+            tmp["ba_code"] = ba_code
+            output = pd.concat([output, tmp])
+        return output
+
+
+class ScaleByBA(BaseEstimator, TransformerMixin):
+    """
+    Applies a given scaler to demand-related columns for each {ba_code} time series.
+
+    This class loops over the unique values of {ba_code} in the dataset and applies
+    the provided scaler to columns related to demand.
+
+    Attributes:
+        scaler (TransformerMixin): The scaler to be applied.
+        fitted_scalers (dict[str, TransformerMixin]): Stores fitted scalers per {ba_code}.
+    """
+
+    def __init__(self, scaler: TransformerMixin) -> None:
+        """
+        Initializes the ScaleByBA with a given scaler.
+
+        Args:
+            scaler: The scaler to be applied.
+        """
+        self.scaler = scaler
+        self.fitted_scalers: dict[str, TransformerMixin] = {}
+
+    def fit(self, X: pd.DataFrame, y: Optional[pd.Series] = None) -> "ScaleByBA":
+        """
+        Fits the scaler to demand-related columns for each {ba_code} time series.
+
+        Args:
+            X: Input dataset.
+            y: Target variable.
+
+        Returns:
+            The fitted instance.
+        """
+        demand_cols = X.filter(like="demand").columns
+        for ba_code in X["ba_code"].unique():
+            tmp = X.loc[X["ba_code"] == ba_code, demand_cols].copy()
+            self.fitted_scalers[ba_code] = (
+                self.scaler.__class__().set_output(transform="pandas").fit(tmp)
+            )
+        return self
+
+    def transform(self, X: pd.DataFrame, y: Optional[pd.Series] = None) -> pd.DataFrame:
+        """
+        Transforms demand-related columns using the fitted scalers for each {ba_code} time series.
+
+        Args:
+            X: Input dataset containing a {ba_code} column.
+            y: Target variable.
+
+        Returns:
+            The transformed dataset.
+        """
+        X_ = X.copy()
+        output = pd.DataFrame()
+        demand_cols = X.filter(like="demand").columns
+        for ba_code in X["ba_code"].unique():
+            tmp = X.loc[X["ba_code"] == ba_code, demand_cols].copy()
+            tmp = self.fitted_scalers[ba_code].transform(tmp)
+            output = pd.concat([output, tmp])
+        X_[demand_cols] = output[demand_cols]
+        return X_
 
 
 def make_baseline_predictions(
