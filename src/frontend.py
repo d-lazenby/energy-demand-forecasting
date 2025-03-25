03c7c318240d7901d34cdc7bfb32e5afbd61ccec
Doug <douglazenby@gmail.com>
2025-03-25 13:25:17 +0000

    Modifying to read predictions from feature store rather than loading model to make them

--- src/frontend.py
+++ src/frontend.py
@@ -4,8 +4,7 @@ from datetime import datetime
 from src.plot import plot_demand_with_prediction
 from src.inference import (
     load_batch_of_features_for_inference,
-    load_model_from_registry,
-    get_model_predictions,
+    load_batch_of_predictions,
 )
 
 st.set_page_config(layout="wide")
@@ -31,38 +30,26 @@ with st.sidebar:
 st.sidebar.header("Working... 🐝")
 progress_bar = st.sidebar.progress(0)
 
-n_steps = 4
+n_steps = 3
 
-with st.spinner(text="Fetching batch of features for inference..."):
+with st.spinner(text="Fetching historical data..."):
     inference_data = load_batch_of_features_for_inference(current_date)
     st.sidebar.write("✅ Features fetched successfully")
     progress_bar.progress(1 / n_steps)
 
-with st.spinner(text="Loading model from registry..."):
-    model, preprocessing_pipeline = load_model_from_registry()
-    st.sidebar.write("✅ Model loaded")
-    progress_bar.progress(2 / n_steps)
-
-with st.spinner(text="Model is making inferences..."):
-    features_end = str(inference_data["datetime"].dt.date.max())
-
-    predictions = get_model_predictions(
-        model=model,
-        preprocessing_pipeline=preprocessing_pipeline,
-        X=inference_data,
-        features_end=features_end,
-    )
+with st.spinner(text="Fetching predictions from the feature store..."):
+    predictions = load_batch_of_predictions(current_date=current_date, days_in_past=2)
     st.sidebar.write("✅ Predictions made")
-    progress_bar.progress(3 / n_steps)
+    progress_bar.progress(2 / n_steps)
 
 with st.spinner(text="Preparing plots..."):
     for ba_code in inference_data["ba_code"].unique():
         tmp_pred = predictions.loc[predictions["ba_code"] == ba_code]
-        pred = (tmp_pred["datetime"].iloc[0], tmp_pred["predicted_demand"].iloc[0])
+        pred = (tmp_pred["datetime"].iloc[-1], tmp_pred["predicted_demand"].iloc[-1])
 
         fig = plot_demand_with_prediction(inference_data, ba_code, pred)
 
         st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
     st.sidebar.write("✅ Plotting complete")
-    progress_bar.progress(4 / n_steps)
+    progress_bar.progress(3 / n_steps)
