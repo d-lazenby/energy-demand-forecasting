ce8140c841990a00b70dd11ce5918f001c091259
Doug <douglazenby@gmail.com>
2025-03-27 14:02:58 +0000

    Adding function to load predictions and actual values from feature store

--- src/monitoring.py
+++ src/monitoring.py
@@ -1,6 +1,31 @@
 import pandas as pd
+from datetime import datetime
 from sklearn.metrics import mean_absolute_error
 from feature_engine.timeseries.forecasting import WindowFeatures
+
+from src.inference import load_batch_of_features, load_batch_of_predictions
+
+
+def load_predictions_and_actual_values_from_feature_store(
+    current_date: datetime.date, days_in_past: int = 30
+):
+    """
+    Loads predictions and actuals for last {days_in_past} days before {current_date} and combines 
+    them into a single dataframe.
+
+    Args:
+        current_date: The date up to which we want the data to run to
+        days_in_past: The number of days before {current_date} we want the data to run from
+
+    Returns:
+        A merged dataframe containing the predictions and actual demand values for each (date, BA) pair.
+    """
+
+    predictions = load_batch_of_predictions(current_date, days_in_past)
+    actuals = load_batch_of_features(current_date, days_in_past)
+
+    return predictions.merge(actuals, on=["datetime", "ba_code"], how="inner")
+
 
 def get_mae_df(monitoring_df: pd.DataFrame) -> pd.DataFrame:
     """
